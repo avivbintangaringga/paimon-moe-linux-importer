158dc99a27b064b101bddb5c544573ec738f6a3d
avivbintangaringga <avivbintangaringga90@gmail.com>
2025-06-16 20:08:31 +0700

    add version detection and comment

--- paimon.sh
+++ paimon.sh
@@ -3,12 +3,21 @@
 source ./paimon.conf
 
 echo "Step 1. Open your gacha log in the game"
-read "Press enter if you have..."
+read -p "Press enter if you have..."
 
-VERSION="2.37.0.0"
+# Check webview version
+VERSION=$(ls -1 "${GAME_PATH}/GenshinImpact_Data/webCaches" | grep "2." | tail -1)
+
+# Data file path
 DATA_PATH="${GAME_PATH}/GenshinImpact_Data/webCaches/${VERSION}/Cache/Cache_Data/data_2"
-BASE_URL=$(strings "$DATA_PATH" | grep "https://gs.hoyoverse.com"| tail -n1 | cut -d "/" -f3-)
+
+# Get base url
+BASE_URL=$(strings "$DATA_PATH" | grep "https://gs.hoyoverse.com"| tail -1 | cut -d "/" -f3-)
+
+# Append log to the base url
 LOG_URL="${BASE_URL}/#/log"
 
 echo "Step 2. Copy the url below"
+
+# Print gacha log url
 echo $LOG_URL
