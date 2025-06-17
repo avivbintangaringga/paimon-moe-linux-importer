9bc8ab9ffa4f1b294063313bb230eea61ea7a51c
avivbintangaringga <avivbintangaringga90@gmail.com>
2025-06-17 21:36:52 +0700

    add game path checks

--- paimon.sh
+++ paimon.sh
@@ -6,28 +6,42 @@ CONFIG_FILE=./config.paimon
 
 . $CONFIG_FILE
 
-echo " ##############################"
-echo " # Paimon.moe Importer Script #"
-echo " ##############################"
-echo ""
+echo "##############################"
+echo "# Paimon.moe Importer Script #"
+echo "##############################"
 
-# Check if game path is set
-if [ -z "$GAME_PATH" ]
-then
+# Set config NAME VALUE
+set_config() {
+  if grep -q "^${1}=" $CONFIG_FILE
+  then 
+    sed -i "s/^${1}=.*/${1}=\"${2//\//\\/}\"/" $CONFIG_FILE
+  else
+    echo "${1}=\"${2}\"" >> $CONFIG_FILE
+  fi
+
+  . $CONFIG_FILE
+}
+
+input_game_path() {
   read -p "Enter absolute game path: " GAME_PATH
 
   # Change game path config
-  sed -i "s/^GAME_PATH=.*/GAME_PATH=\"${GAME_PATH//\//\\/}\"/" $CONFIG_FILE
-fi
+  set_config "GAME_PATH" "${GAME_PATH}"
+}
 
-# Check if game path is exists
-if [ ! -d $GAME_PATH ]
-then
-  echo "ERROR: specified game path is not found!"
-  exit 1
-fi
+# Check if game path is set
+[ -z "$GAME_PATH" ] && echo && input_game_path
 
-echo "Step 1. Open your gacha log in the game"
+# Check if game path is valid
+while [ ! -d "$GAME_PATH/GenshinImpact_Data" ]
+do
+  echo "ERROR: specified game path is not valid!"
+  echo ""
+  input_game_path
+done
+
+echo ""
+echo "Step 1. Open your gacha log in the game. Make sure it is updated."
 read -p "Press enter if you have..."
 
 # Check webview version
@@ -42,7 +56,12 @@ BASE_URL=$(strings "$DATA_PATH" | grep "https://gs.hoyoverse.com"| tail -1 | cut
 # Append log to the base url
 LOG_URL="${BASE_URL}/#/log"
 
+echo ""
 echo "Step 2. Copy the url below"
 
 # Print gacha log url
+echo ""
 echo $LOG_URL
+
+echo ""
+echo "Step 3. Paste into paimon.moe using Method 2"
